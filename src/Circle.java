b2843041811b4150ad478abf7cbfe26709d89770
tobo3199 <tobias.sauter.003@outlook.com>
2021-11-23 10:08:10 +0100

    fix

--- src/Circle.java
+++ src/Circle.java
@@ -13,26 +13,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Circle extends JPanel{
 
-    /*private int x;
-    private int y;
-    private int width;
-    private int height;*/
-
-    /*public Circle( int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }*/
-
-
-
-
     public void paint (Graphics g){
         //super.paint(g);
         setSize(1000, 1000);
-        //g.drawOval(50, 500, 5, 5);
-        //g.fillOval(50, 500, 5, 5);
 
         int circleCount = 3;
         int max = 650;
@@ -40,8 +23,7 @@ public class Circle extends JPanel{
         int x;
         int y;
 
-        //for(int  a= 0; a < 4; a++) {
-            for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
 
                 x = (int) Math.floor(Math.random() * (max - min + 1) + min);
                 y = (int) Math.floor(Math.random() * (max - min + 1) + min);
@@ -49,8 +31,7 @@ public class Circle extends JPanel{
                 System.out.println(y);
                 g.drawOval(x, y, 5, 5);
                 g.fillOval(x, y, 5, 5);
-            }
-        //}
+        }
     }
 
 
