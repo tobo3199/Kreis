6c744949ec42ea87daf66d7322ece50a7a9dd774
tobo3199 <tobias.sauter.003@outlook.com>
2021-11-30 10:12:54 +0100

    multiple circle

--- src/Circle.java
+++ src/Circle.java
@@ -3,6 +3,7 @@ import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.color.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Circle extends JPanel{
@@ -15,24 +16,48 @@ public class Circle extends JPanel{
         int min = 50;
         int x;
         int y;
+        Random rand = new Random();
 
-        ArrayList <Integer> cordinates = new ArrayList<Integer>();
+        ArrayList <Integer> xList = new ArrayList<Integer>();
+        ArrayList <Integer> yList = new ArrayList<Integer>();
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 2; i++) {
 
-            x = (int) Math.floor(Math.random() * (max - min + 1) + min);
-            y = (int) Math.floor(Math.random() * (max - min + 1) + min);
-            System.out.println("xr: " + x);
-            System.out.println("yr: " + y);
-            System.out.println("---------");
-            //g.drawOval(x, y, 5, 5);
+            x = rand.nextInt((max - min) + 1) + min;
+            y = rand.nextInt((max - min) + 1) + min;
+            //x = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            //y = (int) Math.floor(Math.random() * (max - min + 1) + min);
+           // System.out.println("x: " + x);
+            //System.out.println("y: " + y);
+            //System.out.println("---------");
+            g.drawOval(x, y, 5, 5);
             g.fillOval(x, y, 5, 5);
 
-            cordinates.add(x);
-            cordinates.add(y);
+            xList.add(x);
+            yList.add(y);
+        }
+        int xKordi = 0;
+        int yKordi = 0;
+
+        if(xList.get(0) > xList.get(1)){
+            xKordi = xList.get(0) - xList.get(1);
+            System.out.println("xKordi: " + xKordi +
+                    "xList: " + xList.get(0) +
+                    "xList: " + xList.get(1));
+        } else if (xList.get(0) < xList.get(1)){
+            xKordi = xList.get(1) - xList.get(0);
         }
 
-        //cordinates.forEach( (n) -> { System.out.println(n); } );
+        if(yList.get(0) > yList.get(1)){
+            yKordi = yList.get(0) - yList.get(1);
+        } else if (yList.get(0) < yList.get(1)){
+            yKordi = yList.get(1) - yList.get(0);
+        }
+
+        System.out.println(xList);
+
+        g.drawOval(xKordi,yKordi,xKordi,yKordi);
+
     }
 
 
